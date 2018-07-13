4d5f00c809859b70ef67d54ec0093f53775fae44
Vithya Srikumar <vithya@Macbooks-MacBook-Air.local>
2018-07-13 16:56:04 -0400

    day-4

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-for (var i = 0; i < 1000; i++) {
-  document.querySelector('.container').innerHTML += "<div class="box">";
+for (var i = 0; i <= 1000; i++) {
+  document.querySelector('.container').innerHTML += "<div class='box'></div>";
 }
 
-var allBoxes = document.querySelectorAll('box');
+var allBoxes = document.querySelectorAll('.box');
 
 for (var i = 0; i <= allBoxes.length; i = i + 2) {
-  allBoxes[i].style.background.color = blue;
+  allBoxes[i].style.background = "blue";
 }
